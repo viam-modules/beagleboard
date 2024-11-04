af41138007f8447a9187571359b1da91f54258b1
martha-johnston <martha.johnston@viam.com>
2024-11-04 16:19:56 -0500

    add driver code

---
+++ beaglebone/board.go
@@ -0,0 +1,42 @@
+//go:build linux
+
+// Package beaglebone implements a beaglebone based board.
+package beaglebone
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+
+	"go.viam.com/rdk/components/board"
+	"go.viam.com/rdk/components/board/genericlinux"
+	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/resource"
+)
+
+const modelName = "beaglebone"
+
+// Model for viam supported beagleboard beaglebone board.
+var Model = resource.NewModel("viam", "beagleboard", "beaglebone")
+
+func init() {
+	gpioMappings, err := genericlinux.GetGPIOBoardMappings(modelName, boardInfoMappings)
+	var noBoardErr genericlinux.NoBoardFoundError
+	if errors.As(err, &noBoardErr) {
+		logging.Global().Debugw("error getting beaglebone GPIO board mapping", "error", err)
+	}
+
+	resource.RegisterComponent(
+		board.API,
+		Model,
+		resource.Registration[board.Board, *genericlinux.Config]{
+			Constructor: func(
+				ctx context.Context,
+				_ resource.Dependencies,
+				conf resource.Config,
+				logger logging.Logger,
+			) (board.Board, error) {
+				return genericlinux.NewBoard(ctx, conf, genericlinux.ConstPinDefs(gpioMappings), logger)
+			},
+		})
+}
